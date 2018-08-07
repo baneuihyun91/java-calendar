a65b651dbb4f4dfd67c6272b9552a2bc864ce849
euihyun <euihyun@kyusung2com>
2018-08-07 17:37:32 +0900

    년도, 윤달 등 추가

--- src/euihyun/calendar/Calendar.java
+++ src/euihyun/calendar/Calendar.java
@@ -4,30 +4,35 @@ import java.util.Scanner;
 
 public class Calendar {
 	private static final int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	private static final int[] LEAP_MAX_DAYS = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
-	public int maxDaysOfMonth(int month) {
-		return MAX_DAYS[month - 1];
+	public boolean isLeapYear(int year) {
+		if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
+			return true;
+		}
+		return false;
 	}
-	
-	public void printSampleCalendar() {
+
+	public int getMaxDaysOfMonth(int year, int month) {
+		if (isLeapYear(year)) {
+			return LEAP_MAX_DAYS[month - 1];
+		} else {
+			return MAX_DAYS[month - 1];
+		}
+	}
+
+	public void printCalendar(int year, int month) {
+		int maxDay = getMaxDaysOfMonth(year, month);
+
+		System.out.printf("   <<%4d년 %3d월>>\n", year, month);
 		System.out.println("일 월 화 수 목 금 토");
 		System.out.println("--------------------");
-		System.out.println(" 1  2  3  4  5  6  7");
-		System.out.println(" 8  9 10 11 12 13 14");
-		System.out.println("15 16 17 18 19 20 21");
-		System.out.println("22 23 24 25 26 27 28");
-	}
-	
-	public static void main(String[] args) {
-
-		// 숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
-		Calendar cal = new Calendar();
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("달을 입력하세요");
-		int month = scanner.nextInt();
-
-		System.out.printf("%d월은 %d일까지 있습니다.\n", month, cal.maxDaysOfMonth(month));
-		cal.printSampleCalendar();
-		scanner.close();
+		for (int i = 1; i <= maxDay; i++) {
+			System.out.printf("%3d", i);
+			if (i % 7 == 0) {
+				System.out.println();
+			}
+		}
+		System.out.println();
 	}
 }
